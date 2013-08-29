16765c8ebfc2f665b94f94c5b0e0939bd64277c2
Stevan C Wing <stevancw@me.com>
2013-08-29 18:12:31 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rejume::Application.config.secret_key_base = 'd5a75b6e14a7375046059c4e42babdd325762287f5697c1ba466177475fda80c5b665d8b170b56a2a2fa84b2b6ce907cc14574f6cf999141fc9365af1b7df2b3'
