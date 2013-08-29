5f4adab391e212e9cfb96e96d7c940b2e4381277
Stevan C Wing <stevancw@me.com>
2013-08-29 21:13:54 +0100

    Add login link

---
+++ spec/authentication_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature 'Authentication' do
+
+  context "Given I am on the homepage" do
+    scenario "there should be a link to the login form" do
+      visit root_path
+      click_on 'Login'
+
+      expect(current_path).to eq login_path
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rails'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -20,6 +22,9 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+
+  # Include Factory Girl Syntax to simply call to factories
+  config.include FactoryGirl::Syntax::Methods
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
